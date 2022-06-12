5d2308aceb683272d4a2b6b084d6be9966593bf5
The Cloud Sandbox <sandbox@thecloudsandbox.com>
2022-06-12 09:55:04 -0400

    Remove taost function

--- Utility Scripts/MS Cloud Toolbox.ps1
+++ Utility Scripts/MS Cloud Toolbox.ps1
@@ -21,45 +21,6 @@ Function Show-Menu {
 Install-PackageProvider -name Nuget -minimumversion 2.8.5.201 -force
 Set-PSRepository "PSGallery" -InstallationPolicy Trusted
 
-Function New-DotNetToast {
-    [cmdletBinding()]
-    Param(
-        
-        [Parameter(Mandatory, Position = 0)]
-        [String]
-        $Title,
-        [Parameter(Mandatory,Position = 1)]
-        [String]
-        $Message,
-        [Parameter(Position = 2)]
-        [String]
-        $Logo = "C:\Program Files\WindowsPowerShell\Modules\BurntToast\0.6.2\Images\BurntToast.png"
-       
-  
-    )
-}
-  
-#Code for the toast notification  
-    $XmlString = @"
-    <toast>
-      <visual>
-        <binding template="ToastGeneric">
-          <text>$Title</text>
-          <text>$Message</text>
-          <image src="$Logo" placement="appLogoOverride" hint-crop="circle" />
-        </binding>
-      </visual>
-      <audio src="ms-winsoundevent:Notification.Default" />
-    </toast>
-"@
-$AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
-$null = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
-$null = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]
-$ToastXml = [Windows.Data.Xml.Dom.XmlDocument]::new()
-$ToastXml.LoadXml($XmlString)
-$Toast = [Windows.UI.Notifications.ToastNotification]::new($ToastXml)
-[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppId).Show($Toast)
-
 
 
 $Modules = "Microsoft.Graph","AzureAD","MSOnline","Az","ExchangeOnlineManagement" 
@@ -68,7 +29,7 @@ $AllModules = foreach ($Module in $Modules) {
 
     Write-Verbose "Installing $Module...This may take a few minutes" -Verbose
      Install-Module -Name $Module -Force -Verbose
-     New-DotNetToast -Title 'Success' -Message '$Module Installed'
+  
 
 }
 
